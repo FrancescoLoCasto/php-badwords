dedb50d2dcca02f8be1bc904cfd48088a40cdd75
MyUsername <my@email.com>
2023-01-24 14:10:38 +0100

    Esercixe complete

--- controller.php
+++ controller.php
@@ -1,18 +1,53 @@
-<h3>
-   La tua parola è:
 
-   <?php 
-      echo ($_POST['normal']);
-      echo strlen ($_POST['normal']);
-   ?>
-</h3>
+   <div>
+      <p>
+         La tua parola è:
+
+         <?php 
+            echo $_POST['normal'];
+         ?>
+      </p>
+
+      <h3>
+            ed è composta da:
+         <?php 
+               echo strlen ($_POST['normal']);
+            ?>
+      </h3>
+   </div>
+
+   
+
+   <div>
+
+   <h3>
+      <?php 
+            echo $_POST['censured'];
+         ?>
+      </h3>
 
 
-<h3>
-   La tua parola è:
 
-   <?php 
-      echo ($_POST['censured']);
-      echo strlen ($_POST['censured']);
-   ?>
-</h3>
+
+      <h3>
+         ed è composta da:
+      <?php 
+            echo strlen($censuredString);
+         ?>
+      </h3>
+
+      <p>
+
+      Questo è il tuo testo censurato:
+
+         <?php 
+            $censuredString = str_ireplace('lorem', '***', $_POST['censured']);
+            echo $censuredString;
+         ?>
+      </p>
+   </div>
+
+
+
+
+
