71072445867b738a4a5c62a46483675280d4d9c6
MyUsername <my@email.com>
2023-01-24 13:42:21 +0100

    test for print

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>php-badwords</title>
+</head>
+
+
+
+<body>
+   <h1>
+      ciao
+   </h1>
+
+   <h1>
+   <?php 
+      echo 'Ciao'
+   ?>
+   </h1>
+</body>
+
+
+</html>
