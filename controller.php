d4dde162090b39671c8f59dea1dd0576ff5b42f9
MyUsername <my@email.com>
2023-01-24 22:49:17 +0100

    Fix and insert html template

--- controller.php
+++ controller.php
@@ -1,5 +1,14 @@
-
-   <div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>php-badwords</title>
+</head>
+<body>
+   
+<div>
       <p>
          La tua parola è:
 
@@ -45,6 +54,8 @@
    </div>
 
 
+</body>
+</html>
 
 
 
